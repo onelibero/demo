95b9d4f835c971fa2c169a226203673e6dd603f8
onelibero <1792208603@qq.com>
2023-09-18 20:09:48 +0800

    提交了测试类

---
+++ Text/A.java
@@ -0,0 +1,13 @@
+package cdu.gu.demo.Text;
+
+public class A implements B{
+    public static void main(String args[]){
+        int i;
+        A a1=new  A();
+        i =a1.k;
+        System.out.println("i="+i);
+    }
+}
+interface B{
+    int k=10;
+}
